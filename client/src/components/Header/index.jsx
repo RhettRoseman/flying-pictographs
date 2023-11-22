38c54b541c31e8e5ca72e485dd81da819f576630
Andrew Allen <allenkandrew@gmail.com>
2023-11-21 22:02:26 -0500

    font change

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -12,9 +12,9 @@ const Header = () => {
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
-            <h1 className="m-0">Tech Thoughts</h1>
+            <h1 className="m-0">Blog Site</h1>
           </Link>
-          <p className="m-0">Get into the mind of a programmer.</p>
+          <p className="m-0">Whatever this website is gonna be?</p>
         </div>
         <div>
           {Auth.loggedIn() ? (
